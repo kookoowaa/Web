efdf4fa6d4da2be04d56b8a3a07e63dbe1d22eac
Park, Pablo Chanwoo <30887470+kookoowaa@users.noreply.github.com>
2019-01-28 08:42:00 +0900

    how to handle input data

--- htdocs/process_create.py
+++ htdocs/process_create.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 
-print("content-type: text/html; charset=utf-8")
-print()
-
 import cgi
 
 form = cgi.FieldStorage()
 title = form["title"].value
 desc = form['description'].value
-
-print(title)
-print(desc)
 
 openedFile = open('data/'+title, 'w')
 openedFile.write(desc)
