87eaf68de205e9416621f249a60baffbd56562fb
Pablo <kookoowaa@gmail.com>
2019-01-31 23:16:52 +0000

    xss

--- htdocs/index.py
+++ htdocs/index.py
@@ -5,6 +5,8 @@ print()
 import cgi, os, module
 
 cgi_data = module.classification()
+cgi_data['desc'] = cgi_data['desc'].replace('<', '&lt;')
+cgi_data['desc'] = cgi_data['desc'].replace('>', '&gt;')
 
 print('''
 <!doctype html>
