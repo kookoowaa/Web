68863aaa3ec45c4b12ac4e7ea788ae7f99ef1358
Park, Pablo Chanwoo <30887470+kookoowaa@users.noreply.github.com>
2019-02-12 08:54:28 +0900

    presession

--- Django/00-presession.py
+++ Django/00-presession.py
@@ -151,9 +151,102 @@ print(data==b'')
 
 ### ex10- http.client 모듈 POST 방식 요청
 
+host = '127.0.0.1:8000'
+params = urlencode({
+    'language': 'python',
+    'name': '김석훈',
+    'email': 'shkim@naver.com'
+})
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': 'text/plain'
+}
+
+conn = HTTPConnection(host)
+conn.request('POST', '', params, headers)
+resp = conn.getresponse()
+print(resp.status, resp.reason)
+
+data = resp.read()
+print(data.decode('utf-8'))
+
+conn.close()
+
+
 ### ex11- http.client 모듈 PUT 방식 요청
 
+host = '127.0.0.1:8000'
+params = urlencode({
+    'language': 'python',
+    'name': '김석훈',
+    'email': 'shkim@naver.com'
+})
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': 'text/plain'
+}
+
+conn = HTTPConnection(host)
+conn.request('PUT', '', params, headers)
+resp = conn.getresponse()
+print(resp.status, resp.reason)
+
+data = resp.read()
+print(data.decode('utf-8'))
+
+conn.close()
+
+
 ### ex12- http.client 모듈 예제
+
+import os
+from http.client import HTTPConnection
+from urllib.parse import urljoin, urlunparse
+from urllib.request import urlretrieve
+from html.parser import HTMLParser
+
+class ImageParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if tag != 'img':
+            return
+        if not hasattr(self, 'result'):
+            self.result = []
+        for name, value in attrs:
+            if name== 'src':
+                self.result.append(value)
+
+def download_image(url, data):
+    if not os.path.exists("DOWNLOAD"):
+        os.makedirs("DOWNLOAD")
+    parser = ImageParser()
+    parser.feed(data)
+    dataSet = set(x for x in parser.result)
+
+    for x in sorted(dataSet):
+        imageUrl = urljoin(url, x)
+        basename = os.path.basename(imageUrl)
+        targetFile = os.path.join("DOWNLOAD", basename)
+
+        print("Downloading...", imageUrl)
+        urlretrieve(imageUrl, targetFile)
+
+def main():
+    host = 'www.google.co.kr'
+
+    conn = HTTPConnection(host)
+    conn.request("GET", '')
+    resp = conn.getresponse()
+
+    charset = resp.msg.get_param("charset")
+    data = resp.read().decode(charset)
+    conn.close()
+
+    print('\n>>>>>>>>>>>> Download Images from ', host)
+    url = urlunparse(('http', host, '', '', '', ''))
+    download_image(url, data)
+
+if __name__ =='__main__':
+    main()
 
 ### ex13- 웹서버
 
