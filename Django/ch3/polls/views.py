03cf9aaa28ef840d8032bfe61807d99f02516ea7
Park, Pablo Chanwoo <30887470+kookoowaa@users.noreply.github.com>
2019-02-26 08:57:12 +0900

    Update views.py

--- Django/ch3/polls/views.py
+++ Django/ch3/polls/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from polls.models import Question
 
+# Create your views here.
+
 def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-# Create your views here.
+
+def detail(request, question_id):
+    question = get_object_or404(Question, pk=question_id)
+    return render(request, 'polls/detail.html,' {'question': question})
+
