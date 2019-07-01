fa9c8cd4015c6168f328abbfc7a408e034948f6f
Park, Pablo Chanwoo <kookoowaa@gmail.com>
2019-07-02 00:04:42 +0900

    time, host, static settings

--- Django-Girls/mysite/settings.py
+++ Django-Girls/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cotas-8h9!011kh=#g)2$*375%f!ewk0+v_%dbd2=*m&ej^me&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
